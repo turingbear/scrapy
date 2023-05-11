8669598eea346feaa01a5b58fac9e556ae0b2315
oroiba <oroiba@proton.me>
2023-05-11 09:44:22 -0700

    pipline processing, xpath spider fix

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,9 +10,9 @@ class BookscraperPipeline:
             ## Strip all whitespace from strings
             field_names = adapter.field_names()
             for field_name in field_names:
-                if field_name != 'description':
+                if field_name != ['description']:
                     value = adapter.get(field_name)
-                    adapter[field_name] = value.strip()
+                    adapter[field_name] = value[0].strip()
             
             ## Category & Product Type --> switch to lowercase
             lowercase_keys = ['category', 'product_type']
